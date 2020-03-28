86a4476a3015613c001609cd5c43b018a392caa2
coco961003 <42399747+coco961003@users.noreply.github.com>
2020-03-28 22:44:29 +0000

    Add FGSM code comments

--- fgsm/FGSM.py
+++ fgsm/FGSM.py
@@ -7,7 +7,14 @@ import numpy as np
 
 loss_object = tf.keras.losses.CategoricalCrossentropy()
 
-
+'''
+The first step is to create perturbations which will be used to distort the original image
+resulting in an adversarial image. As mentioned, for this task, the gradients are taken with respect to the image.
+input_image: the target image or named original image
+input_label: the label of the target image in the target model
+model: the target model which would be attacked
+isVerbose: output the process, default is False
+'''
 def create_adversarial_pattern(input_image, input_label, model: Model, isVerbose=False):
     with tf.GradientTape() as tape:
         tape.watch(input_image)
@@ -21,23 +28,41 @@ def create_adversarial_pattern(input_image, input_label, model: Model, isVerbose
     signed_grad = tf.sign(gradient)
     return signed_grad
 
-
+'''
+according the target image to generate perturbations
+perturbations can be output
+input_image: the target image or named original image
+model: the target model which would be attacked
+isVerbose: output the process, default is False
+useMatplotLib: show perturbations in plt, default is False
+'''
 def create_perturbations(input_image, model: Model, isVerbose=False, useMatplotLib=False):
     if isVerbose:
         print('Get the input label of the image.')
+    # get the input label index of the image
     input_image_index = int(util.getPredictedLabel(util.predictedLabelToMap(model.predict(input_image))))
     input_image_probs = model.predict(input_image)
+    # convert input into one-hot type data output
     label = tf.one_hot(input_image_index, input_image_probs.shape[-1])
     label = tf.reshape(label, (1, input_image_probs.shape[-1]))
     perturbations = create_adversarial_pattern(input_image, label, model, isVerbose)
-    # comment these three lines if u don't want pictures
+    # make resulting perturbations visible
     if useMatplotLib:
         plt.figure
         plt.imshow(perturbations[0])
         plt.show()
     return perturbations
 
-
+'''
+generate adversarial examples by adding small perturbations to the original image
+it also trys to find the minimum perturbations by while loop
+CAREFUL: the value of epsilon effects accuracy and efficiency, but it is not used as an input to this function
+input_image: the target image or named original image
+model: the target model which would be attacked
+isVerbose: output the process, default is False
+useMatplotLib: show adversarial examples in plt, default is False
+maxIter: Maximum number of iterations, default 500
+'''
 def fgsm_attack(input_image, model: Model, isVerbose=False, useMatplotLib=False, maxIter=500):
     epsilon = 0
     iterCount = 0
@@ -50,6 +75,7 @@ def fgsm_attack(input_image, model: Model, isVerbose=False, useMatplotLib=False,
         if isVerbose:
             print('Now epsilon is ', epsilon)
             print('Now iterCount is ', iterCount)
+        # score calculation for FGSM
         totalPert = np.sum((epsilon * np.abs(perturbations))) + totalPert
         adv_x = input_image + (epsilon * perturbations)
         adv_x = tf.clip_by_value(adv_x, 0, 1)
@@ -59,5 +85,5 @@ def fgsm_attack(input_image, model: Model, isVerbose=False, useMatplotLib=False,
             plt.show()
         prediction = int(util.getPredictedLabel(util.predictedLabelToMap(model.predict(adv_x))))
         iterCount = iterCount + 1
-        epsilon = 0.1 * iterCount  # change the value(increase 0.01) to make the different obvious
+        epsilon = 0.1 * iterCount  # increase the value of epsilon to make the difference obvious
     return totalPert, adv_x, prediction != initPrediction
